5ec777f2beeded2f933ca59556f15f61312ad5f4
Golam Rabbani <knight3rrantt@gmail.com>
2017-02-04 02:28:40 +0600

    empty init

--- App.js
+++ App.js
@@ -9,7 +9,7 @@ import {
   TextInput
 } from 'react-native';
 
-
+import {Scene} from 'react-native-router-flux';
 import { GiftedChat } from 'react-native-gifted-chat';
 
 export default class Index extends Component {
